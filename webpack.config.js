77888b1525e5664e512396bf8450fdb6d296309b
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2021-08-31 23:32:37 +0500

    Removed log, unused package.

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,45 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = (env, options) => {
-    console.log('my output mode: ', options.mode);
-    return {
-        mode: options.mode,
-        context: path.resolve(__dirname, 'src'),
-        entry: './main.js',
-        output: {
-            filename: '[name].js',
-            path: path.resolve(__dirname, './build')
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: [path.resolve(__dirname, 'node_modules')],
-                    loader: 'babel-loader'
-                },
-                {
-                    test: /\.vue$/,
-                    loader: 'vue-loader'
-                },
-                {
-                    test: /\.css$/i,
-                    use: ['style-loader', 'css-loader']
-                }
-            ]
-        },
-        resolve: {
-            extensions: ['', '.js', '.jsx']
-        },
-        devServer: {
-            port: 3000,
-            static: path.join(__dirname, 'dist')
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new VueLoaderPlugin(),
-            new HTMLWebpackPlugin({
-                title: 'Micro Frontends',
-                template: path.resolve(__dirname, './src/index.html'),
-                filename: 'index.html'
-            })
+module.exports = (env, options) => ({
+    mode: options.mode,
+    context: path.resolve(__dirname, 'src'),
+    entry: './main.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [path.resolve(__dirname, 'node_modules')],
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
         ]
-    };
-};
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        port: 3000,
+        static: path.join(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new VueLoaderPlugin(),
+        new HTMLWebpackPlugin({
+            title: 'Micro Frontends',
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html'
+        })
+    ]
+});
